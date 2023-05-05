2b50ca3355863803114b6369f6805d89d883c443
Cierra Ray <cierraray2@gmail.com>
2023-05-05 15:32:53 -0400

    fixed root

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'states.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
